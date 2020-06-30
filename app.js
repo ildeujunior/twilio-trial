7b7c218a78f03c25b2b9b79a8f8a496441a750e2
ildeujunior <ildeujunior1991@hotmail.com>
2020-06-30 09:03:50 -0300

    webhook with ngrok, twiml

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const twilio = require('twilio')
 require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const MessagingResponse = twilio.twiml.MessagingResponse
+
+app.use(bodyParser.urlencoded({extended:false}))
 
 const twilioClient = twilio(
     process.env.TWILIO_ACCOUNT_SID,
     process.env.TWILIO_AUTH_TOKEN
 )
 
-twilioClient.messages.create({
-    from: '+13342747580',
-    to: '+5511952340157',
-    body: 'First Message, DevZuno!'
-}).then(console.log).catch(console.error);
+app.get('/', (req, res) => {
+
+    twilioClient.messages.create({
+        from: 'whatsapp:+14155238886',
+        to: 'whatsapp:+5511952340157',
+        body: 'Your appointment is coming up on Jan 30 at 11h'
+    }).then(console.log).catch(console.error)
+})
+
+app.post('/webhook', (req, res) => {
+    const msg = req.body.Body;
+    const twiml = new MessagingResponse();
+    twiml.message(`Obrigado por enviar a mensagem ${msg}`);
+    res.send(twiml.toString())
+})
+
+app.listen(3000, () => console.log('Application running on 3000'))
