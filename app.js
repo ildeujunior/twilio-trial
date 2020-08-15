19720154e112abf9234705cbd6e44ab7237f79f4
ildeujunior <ildeujunior1991@hotmail.com>
2020-08-15 00:58:33 -0300

    methods with TwilioQuest

--- app.js
+++ app.js
@@ -1,32 +1,68 @@
-const twilio = require('twilio')
 require('dotenv').config()
+const twilio = require('twilio')
 const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
 const MessagingResponse = twilio.twiml.MessagingResponse
+const bodyParser = require('body-parser')
 
+
+const app = express()
 app.use(bodyParser.urlencoded({extended:false}))
 
-const twilioClient = twilio(
-    process.env.TWILIO_ACCOUNT_SID,
-    process.env.TWILIO_AUTH_TOKEN
-)
+var TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID,
+    TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN,
+    TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
 
-app.get('/', (req, res) => {
+var twilioClient = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+app.get('/message', (req, res) => {
 
     twilioClient.messages.create({
-        from: 'whatsapp:+14155238886',
-        to: 'whatsapp:+5511952340157',
-        body: 'Your appointment is coming up on Jan 30 at 11h'
+        from: TWILIO_PHONE_NUMBER,
+        to: '+5511952340157',
+        body: 'Olá Mundão'
     }).then(console.log).catch(console.error)
 })
 
-app.post('/webhook', (req, res) => {
-    const msg = req.body.Body
-    const twiml = new MessagingResponse()
-    twiml.message(`Obrigado por enviar a mensagem ${msg}`)
-    console.log(twiml.toString())
-    res.send(twiml.toString())
-})
+app.post('/call', function(req, res, next) {
+    twilioClient.calls
+    .create({
+       url: 'http://demo.twilio.com/docs/voice.xml',
+       to: '+5511952340157',
+       from: TWILIO_PHONE_NUMBER
+     })
+    .then(call => console.log(call.sid));
+  });
 
-app.listen(3000, () => console.log('Application running on 3000'))
+  app.post('/webhook', (req, res) => {
+      const msg = req.body.Body
+      const twiml = new MessagingResponse()
+      twiml.message(`Obrigado por enviar essa mensagem ${msg}`)
+      res.send(twiml.toString())
+  })
+
+
+  // Create a TwiML document to provide instructions for an outbound call
+app.post('/hello', function(req, res, next) {
+    // Create a TwiML generator
+    var twiml = new twilio.twiml.VoiceResponse();
+    // var twiml = new twilio.TwimlResponse();
+    twiml.say('Hello there! You have successfully configured a web hook.');
+    twiml.say('Good luck on your Twilio quest!', { 
+        voice:'woman' 
+    });
+  
+    // Return an XML response to this request
+    res.set('Content-Type','text/xml');
+    res.send(twiml.toString());
+  });
+
+
+
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+    console.log(`HTTP server listening on http://localhost:${PORT}/`);
+    console.log(`Paste this URL into the hack UI to complete the challenge!`);
+    console.log(`You can open this URL in your web browser too to test it out.`);   
+})
